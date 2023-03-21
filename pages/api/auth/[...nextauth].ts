dadc2779d692a6a415c262c130ce72a768854943
viloriajer <viloria.jer@gmail.com>
2023-03-20 22:45:51 -0600

    update: mock credentials

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,14 +1,36 @@
 import NextAuth from "next-auth"
+import Email from "next-auth/providers/email"
 import GithubProvider from "next-auth/providers/github"
-
+import Credentials from "next-auth/providers/credentials"
 export const authOptions = {
-  
+  secret:'1234',
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
     }),
-
+    Credentials({
+      name:'Credentials',
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        password: { label: "Password", type: "password" }
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+  
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null
+  
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      }
+  
+    })
   ],
 }
 
