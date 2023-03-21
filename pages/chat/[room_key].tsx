582340259e2eace041f466d4e08ff4dc3f91cf34
viloriajer <viloria.jer@gmail.com>
2023-03-20 20:43:59 -0600

    update: create chat room

---
+++ pages/chat/[room_key].tsx
@@ -0,0 +1,11 @@
+import { useRouter } from 'next/router'
+import React from 'react'
+
+const ChatRoom = () => {
+    const router = useRouter()
+  return (
+    <div>ChatRoom</div>
+  )
+}
+
+export default ChatRoom
