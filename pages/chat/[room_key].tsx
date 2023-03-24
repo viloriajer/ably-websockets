fe5187a781ae0c9ca01b2424550f39d776bfbf32
viloriajer <viloria.jer@gmail.com>
2023-03-23 21:20:37 -0600

    update: styles, function to scroll when overflowing

--- pages/chat/[room_key].tsx
+++ pages/chat/[room_key].tsx
@@ -1,12 +1,17 @@
 import { configureAbly, useChannel, usePresence } from '@ably-labs/react-hooks';
-import { useSession } from 'next-auth/react';
+import { useSession,signOut } from 'next-auth/react';
+import Link from 'next/link';
 import { useRouter } from 'next/router'
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 
 function ListActiveParticipants({members}){
     return <div className='flex flex-col w-32'>
+        <Link href={'/join-session'}>Back</Link>
         {members}
+        <button className='mt-auto btn btn-error' onClick={()=>signOut({callbackUrl:'/login'})}>
+            Sign Out
+        </button>
     </div>
 }
 
@@ -18,16 +23,31 @@ const ChatRoom = () => {
     configureAbly({authUrl:'/api/ably/auth'})
     const [message,setMessage] = useState('')
     const [messages, updateMessages] = useState([]);
+    const [displayToast,setDisplayToast] = useState(false)
+    const [toastMessage,setToastMessage] = useState('')
     const [channel] = useChannel(room_key ?? '', (message) => {
-        updateMessages((prev) => [...prev, message]);
+        if (message.data.message)
+            updateMessages((prev) => [...prev, message]);
     });
-    const [presenceData,updateStatus] = usePresence(room_key ?? '', {name: session?.user?.name, typing:false}, ({action})=> {
-        if (action=='leave')
-        console.log('user left')
+    const [presenceData,updateStatus] = usePresence(room_key ?? '', {name: session?.user?.name}, ({action,data})=> {
+        console.log(action)
+        if (action==='leave'){
+
+            setDisplayToast(true)
+            setToastMessage(`${data.name} has left.`)
+            return 
+        }
+        if(action === 'enter') {
+            setDisplayToast(true)
+            setToastMessage(`${data.name} has joined.`)
+            return
+        }
     });
 
     const members = presenceData.map((msg, index) => <p key={index}> {msg.data.name}</p>);
 
+    
+    console.log(channel)
     function onChangeMessageBox(e){
         updateStatus({name:session?.user?.name, typing:true})
         setMessage(e.target.value)
@@ -43,21 +63,56 @@ const ChatRoom = () => {
         })
     }
 
+    function handleKeyDown(e){
+        if (e.keyCode === 13){
+            channel.publish('NEW_MESSAGE', {message,author:session?.user?.name},(err)=>{
+            if(err)
+                console.log(err)
+            else {
+                setMessage('')
+            }
+        })
+        }
+    }
+
+    useEffect(() => {
+        const displayTimer =
+        setTimeout(()=>{
+            setDisplayToast(false)
+        },2000)
+
+        return () => clearTimeout(displayTimer)
+    },[displayToast])
+
+
+    const messagesEndRef = useRef(null)
+
+    const scrollToBottom = () => {
+      messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
+    }
+  
+    useEffect(() => {
+        scrollToBottom()
+      }, [messages]);
     
   return (
     <div className='h-screen flex p-8'>
         <ListActiveParticipants members={members}/>
         <div className='flex flex-col h-full w-full px-16'>
-            <div className='flex flex-col h-full w-full overflow-scroll py-4'>
+            <div id='messages' className='flex relative flex-col h-full w-full overflow-scroll py-4'>
                 {messages.map(({data}, index)=> (
                     <div className={`chat ${data?.author == session?.user?.name ? 'chat-end' : 'chat-start'}`}key={index}>
                         <p className="chat-header">{data?.author == session?.user?.name ? 'You' : data?.author}</p>
                         <p className="chat-bubble">{data?.message}</p>
                     </div>
                 ))}
+                {displayToast&&
+                <p className='alert shadow-lg absolute bottom-10 w-1/5 left-1/3'>{toastMessage}</p>
+                 }
+                 <div ref={messagesEndRef} />
             </div>
             <div className='flex w-full'>
-                <input className='input input-bordered w-full mr-2' type="text" onChange={onChangeMessageBox} value={message}/>
+                <input className='input input-bordered w-full mr-2' type="text" onChange={onChangeMessageBox} value={message} onKeyDown={handleKeyDown}/>
                 <button className='btn btn-success' onClick={onClickSend}>send</button>
             </div>
         </div>
--- pages/join-session/index.tsx
+++ pages/join-session/index.tsx
@@ -8,17 +8,20 @@ const JoinSessionPage = () => {
     const { push } = useRouter();
 
     function onClickJoin(){
-      if(channels.indexOf(roomCode) !== -1) 
+      if(channels.indexOf(roomCode) !== -1) {
         push(`chat/${roomCode}`)
+      }
       setRoomMissing(true)
     }
     
   return (
-    <div className='flex flex-col h-screen items-center justify-center'>
-        <div className='flex flex-col'><p>Please enter a room code</p>
-        <input type="text" onChange={(e)=>setRoomCode(e.target.value)}/></div>
-        <button onClick={onClickJoin}>join</button>
-        {roomMissing && <p>room does not exist</p> }
+    <div className='flex flex-col h-screen items-center justify-center bg-gray-100'>
+        <div className='flex flex-col items-center justify-between shadow-lg w-1/2 h-1/3 p-4 rounded-md bg-white'>
+        {roomMissing && <p className='text-red-600'>room does not exist</p> }
+          <p className='text-lg'>Please enter a room code</p>
+        <input type="text" className='input input-bordered' onChange={(e)=>setRoomCode(e.target.value)}/>
+        <button className='btn btn-success' onClick={onClickJoin}>join</button>
+        </div>
     </div>
   )
 }
